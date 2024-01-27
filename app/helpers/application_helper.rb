45d5607e0194e1d5cc635ce9d3378d9919a534fb
takaya <takaya314boxing@gmail.com>
2024-01-27 04:38:21 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '') # full_titleメソッドを定義、デフォルトで空のページタイトルを受け取る
+    base_title = 'TwotterApp'      # アプリケーションの基本となるタイトルを設定
+    if page_title.empty?           # もしページタイトルが空なら
+      base_title                   # 基本のタイトルのみを返す
+    else                           # そうでなければ
+      "#{page_title} | #{base_title}" # ページタイトルと基本のタイトルを連結して返す
+    end
+  end
 end
